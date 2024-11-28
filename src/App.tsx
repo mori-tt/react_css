497d12da70ccae80c7bd586c27a0194d46d2afa7
T_Mori <morit0301@gmail.com>
2024-11-28 12:19:17 +0900

    Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
 import StyledJSX from "./components/StyledJSX";
+import { StyledComponents } from "./components/StyledComponents";
+
 function App() {
   return (
     <div>
       <InlineStyle />
       <CssModules />
       <StyledJSX />
+      <StyledComponents />
     </div>
   );
 }
