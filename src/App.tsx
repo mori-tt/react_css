6ca0873ba182706682f7574a5cfd2d702048ccf7
T_Mori <morit0301@gmail.com>
2024-11-28 11:21:48 +0900

    Inline Styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { InlineStyle } from "./components/InlineStyle";
+
+function App() {
+  return (
+    <div>
+      <InlineStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InlineStyle.tsx
@@ -0,0 +1,29 @@
+export const InlineStyle = () => {
+  const cotainerStyle = {
+    border: "solid 2px #382eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+  };
+
+  const titleStyle = {
+    martin: 0,
+    color: "#3d84a8",
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px",
+  };
+  return (
+    <div style={cotainerStyle}>
+      <p style={titleStyle}>- InlineStyle -</p>
+      <button style={buttonStyle}>FIGHT!</button>
+    </div>
+  );
+};
